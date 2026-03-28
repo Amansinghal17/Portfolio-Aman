4462acf7535e5f474b5d99010cfa00e716b95a28
Aman Singhal <amansinghal1702@gmail.com>
2026-03-28 14:53:24 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default {
-  base: '/Portfolio-Aman/',
+  base: "/Portfolio-Aman/",
+  build: { outDir: "docs" },
   plugins: [react(), tailwindcss()],
 }
