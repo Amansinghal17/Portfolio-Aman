e72f944c5a09589fc61146f8d1e4fca53d1718d0
Aman Singhal <amansinghal1702@gmail.com>
2026-03-29 14:33:24 +0530

    solved the issue in configration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default {
-  base: "/Portfolio-Aman/",
+  base: process.env.NODE_ENV === 'production' ? "/Portfolio-Aman/" : "/",
   build: { outDir: "docs" },
   plugins: [react(), tailwindcss()],
 }
